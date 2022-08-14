074d9fce8a4ee137ba64cbad92f0942fc8102ddf
Revelenchant <89518310+Revelenchant@users.noreply.github.com>
2022-08-14 23:47:05 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict"
+
+/*let first = document.querySelector('.page');
+
+
+let themeButton = document.querySelector('.theme-button');
+themeButton.onclick = function() {
+    first.classList.toggle('light-theme');
+    first.classList.toggle('dark-theme');
+};
+*/
+
+const headers = document.querySelectorAll("[data-name='spoiler-title']");
+headers.forEach(function(item) {
+    item.addEventListener("click", headerClick);
+});
+function headerClick() {
+    this.nextElementSibling.classList.toggle("spoiler-body");
+}
